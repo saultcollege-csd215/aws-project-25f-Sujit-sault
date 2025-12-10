cc61c276a3f9ba52f3d3e462d9ff7230085edbe8
Sujit Bolakhe <23079797@saultcollege.ca>
2025-12-10 18:41:36 +0000

    Add CI/CD workflows and deployment scripts

--- resources/deploy_ec2.sh
+++ resources/deploy_ec2.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# This gets run ON the EC2 instance (NOT in the GitHub Actions runner)
-
 cd /home/ec2-user/dice
-
-git pull
-
+git pull origin AWS
 sudo systemctl restart diceapp
-sudo systemctl status diceapp --no-pager -l
