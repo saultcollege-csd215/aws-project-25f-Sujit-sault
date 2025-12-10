e42af449cb0af60a580afb5810dd96422726b608
Sujit Bolakhe <23079797@saultcollege.ca>
2025-12-10 19:13:04 +0000

    Add deploy_lambda.sh script

--- resources/deploy_lambda.sh
+++ resources/deploy_lambda.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 LAMBDA_NAME="csd215-lambda"
-echo "Step 1: Packaging Lambda function..."
 cd app
 zip -r ../lambda-deploy.zip .
-cd ..
-echo "Step 2: Updating Lambda function code..."
-aws lambda update-function-code \
-    --function-name $LAMBDA_NAME \
-    --zip-file fileb://lambda-deploy.zip
-echo "Step 3: Lambda deployment complete!"
+aws lambda update-function-code --function-name $LAMBDA_NAME --zip-file fileb://../lambda-deploy.zip
